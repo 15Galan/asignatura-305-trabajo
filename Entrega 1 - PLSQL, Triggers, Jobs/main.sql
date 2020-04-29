52ef05148a45ff91d9b8e1434516f6b98800c698
unknown <quino.terrasa+dev@gmail.com>
2020-04-29 16:44:12 +0200

    Tabla del ejercicio 2 creada.

--- Entrega 1 - PLSQL, Triggers, Jobs/main.sql
+++ Entrega 1 - PLSQL, Triggers, Jobs/main.sql
@@ -1,9 +1,18 @@
 -- Practica en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
 -- Javier Galán, Manuel González, Pablo Rodríguez, Joaquin Terrasa
 
+-- { Por defecto, usamos el usuario "AUTORACLE" creado previamente en la BD }
+
 -- [1]
 
 -- [2]
+CREATE TABLE COMPRA_FUTURA (
+    PROVEEDOR_NIF VARCHAR2(16 BYTE),
+    TELEFONO NUMBER(*, 0),
+    NOMBRE VARCHAR2(64 BYTE),
+	EMAIL VARCHAR2(64 BYTE),
+    CANTIDAD NUMBER(*, 0),
+    CODREF_PIEZA NUMBER(*, 0));
 
 -- [3]
 
