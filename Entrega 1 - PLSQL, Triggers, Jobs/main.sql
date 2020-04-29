c9a8fb700ebdd4ba41376ad75178149aadcc6fd2
unknown <quino.terrasa+dev@gmail.com>
2020-04-29 16:39:41 +0200

    Agrego la plantilla para la parte de PLSQL. Agrego una rama para trabajar en esta parte.

---
+++ Entrega 1 - PLSQL, Triggers, Jobs/main.sql
@@ -0,0 +1,18 @@
+-- Practica en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
+-- Javier Galán, Manuel González, Pablo Rodríguez, Joaquin Terrasa
+
+-- [1]
+
+-- [2]
+
+-- [3]
+
+-- [4]
+
+-- [5]
+
+-- [6]
+
+-- [7]
+
+-- [8]
