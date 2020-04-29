57f2ac7152a8718ad79d629ead609a00aadc92b2
Galán <antoniogalanG15@gmail.com>
2020-04-29 18:26:17 +0200

    Entrega 1 modificada (ejercicio 1)
    Creación de los roles pedidos, pero queda restringir algunos permisos.

--- Entrega 1 - PLSQL, Triggers, Jobs/main.sql
+++ Entrega 1 - PLSQL, Triggers, Jobs/main.sql
@@ -1,9 +1,92 @@
--- Practica en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
--- Javier Gal√°n, Manuel Gonz√°lez, Pablo Rodr√≠guez, Joaquin Terrasa
+-- Trabajo en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
+-- Antonio J. Gal·n, Manuel Gonz·lez, Pablo RodrÌguez, Joaquin Terrasa
 
 -- { Por defecto, usamos el usuario "AUTORACLE" creado previamente en la BD }
 
 -- [1]
+CREATE ROLE r_administrativo;
+CREATE ROLE r_mecanico;
+CREATE ROLE r_cliente;
+
+
+GRANT dba
+    TO r_administrativo;
+
+
+GRANT SELECT
+    ON autoracle.empleado
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.categoria
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.compatible
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.examen
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.mantenimiento
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.marca
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.modelo
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.necesita
+    TO r_mecanico;
+    
+GRANT SELECT
+    ON autoracle.pieza
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.reparacion
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.requiere
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.trabaja
+    TO r_mecanico;
+
+GRANT SELECT
+    ON autoracle.vacaciones
+    TO r_mecanico;
+
+
+GRANT SELECT
+    ON autoracle.cliente
+    TO r_cliente;
+
+
+GRANT SELECT
+    ON autoracle.cita
+    TO r_mecanico, r_cliente;
+    
+GRANT SELECT
+    ON autoracle.factura
+    TO r_mecanico, r_cliente;
+    
+GRANT SELECT
+    ON autoracle.servicio
+    TO r_mecanico, r_cliente;
+
+GRANT SELECT
+    ON autoracle.vehÌculo
+    TO r_mecanico, r_cliente;
+
 
 -- [2]
 CREATE TABLE COMPRA_FUTURA (
@@ -13,6 +96,7 @@ CREATE TABLE COMPRA_FUTURA (
 	EMAIL VARCHAR2(64 BYTE),
     CANTIDAD NUMBER(*, 0),
     CODREF_PIEZA NUMBER(*, 0));
+
 
 -- [3]
 
