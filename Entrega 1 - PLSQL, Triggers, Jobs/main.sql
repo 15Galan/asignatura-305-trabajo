a97f52d5d4ca22021c5a55a0a280943c9bc6803e
unknown <quino.terrasa+dev@gmail.com>
2020-04-30 13:58:26 +0200

    Resolucion del modelo ER aumentado. He añadido politicas de privacidad (EJ. 1) para clientes y empleados, aunque me da errores.

--- Entrega 1 - PLSQL, Triggers, Jobs/main.sql
+++ Entrega 1 - PLSQL, Triggers, Jobs/main.sql
@@ -3,15 +3,41 @@
 
 -- { Por defecto, usamos el usuario "AUTORACLE" creado previamente en la BD }
 
--- [1]
+-- [1] Modificar el modelo (si es necesario) para almacenar el usuario de Oracle que cada empleado o cliente pueda
+-- utilizar para conectarse a la base de datos. Además, habrá de crear roles dependiendo del tipo de usuario:
+--   Administrativo, con acceso a toda la BD;
+--   Empleado, con acceso sólo a aquellos objetos que precise para su trabajo
+--     (y nunca podrá acceder a los datos de otros empleados);
+--   Cliente, con acceso sólo a los datos propios, de su vehículo y de sus servicios.
+-- Los roles se llamarán R_ADMINISTRATIVO, R_MECANICO, R_CLIENTE.
+
+-- ||||| desde SYSDBA |||||
+
+-- Antes de comenzar, asignaremos USUARIO1 y USUARIO2 (de las practicas anteriores) a las tablas EMPLEADO y CLIENTE
+-- respectivamente, para poder probar los cambios que vamos a realizar en la BD.
+SELECT username, user_id FROM ALL_USERS;
+-- en mi caso, ID(USUARIO1) = 104 e ID(USUARIO2) = 105
+
+UPDATE AUTORACLE.CLIENTE
+    SET IDCLIENTE = 104 WHERE IDCLIENTE = 111;
+
+UPDATE AUTORACLE.EMPLEADO
+    SET IDEMPLEADO = 105 WHERE IDEMPLEADO = 300;
+
 CREATE ROLE r_administrativo;
 CREATE ROLE r_mecanico;
 CREATE ROLE r_cliente;
 
+GRANT R_CLIENTE TO USUARIO1;
+GRANT R_MECANICO TO USUARIO2;
+GRANT R_ADMINISTRATIVO TO AUTORACLE;
+
+-- { permisos para R_ADMINISTRATIVO }
 
 GRANT dba
     TO r_administrativo;
 
+-- { permisos para R_MECANICO }
 
 GRANT SELECT
     ON autoracle.empleado
@@ -65,11 +91,67 @@ GRANT SELECT
     ON autoracle.vacaciones
     TO r_mecanico;
 
+-- agregamos una politica VPD (ver practica 3 / tema 2) para limitar el acceso a los datos de cada empleado
+-- agregamos restricciones a las tablas "EMPLEADO", "VACACIONES", "FACTURA" y "TRABAJA".
+SELECT * FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME LIKE '%EMPLEADO%';
+-- permite precisar qué tablas dependen de EMPLEADO_ID
+
+-- devuelve un filtro para la clausula WHERE
+-- https://www.techonthenet.com/oracle/functions/sys_context.php
+CREATE OR REPLACE FUNCTION AUTORACLE.SOLO_EMPLEADO_ACTUAL_EMPLEADO (p_esquema IN VARCHAR2, p_objeto IN VARCHAR2)
+    RETURN VARCHAR2 AS
+BEGIN
+    IF (SYS_CONTEXT('userenv', 'isdba') = 'TRUE') THEN
+        RETURN '';
+    ELSE
+        RETURN 'IDEMPLEADO = ''' || SYS_CONTEXT('userenv', 'session_userid') || '''';
+    END IF;
+END;
+
+CREATE OR REPLACE FUNCTION AUTORACLE.SOLO_EMPLEADO_ACTUAL_VAC_TRA_FAC (p_esquema IN VARCHAR2, p_objeto IN VARCHAR2)
+    RETURN VARCHAR2 AS
+BEGIN
+    IF (SYS_CONTEXT('userenv', 'isdba') = 'TRUE') THEN
+        RETURN '';
+    ELSE
+        RETURN 'EMPLEADO_IDEMPLEADO = ''' || SYS_CONTEXT('userenv', 'session_userid') || '''';
+    END IF;
+END;
+
+BEGIN
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'EMPLEADO', policy_name => 'POL_EMPLEADO_EMPLEADO',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_EMPLEADO_ACTUAL_EMPLEADO', statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'VACACIONES', policy_name => 'POL_EMPLEADO_VACACIONES',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_EMPLEADO_ACTUAL_VAC_TRA_FAC', statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'TRABAJA', policy_name => 'POL_EMPLEADO_TRABAJA',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_EMPLEADO_ACTUAL_VAC_TRA_FAC', statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'FACTURA', policy_name => 'POL_EMPLEADO_FACTURA',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_EMPLEADO_ACTUAL_VAC_TRA_FAC', statement_types => 'SELECT'
+    );
+END;
+/
+
+-- en el caso de que te equivoques:
+BEGIN
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'EMPLEADO', 'POL_EMPLEADO_EMPLEADO');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'VACACIONES', 'POL_EMPLEADO_VACACIONES');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'TRABAJA', 'POL_EMPLEADO_TRABAJA');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'FACTURA', 'POL_EMPLEADO_FACTURA');
+END;
+/
+
+-- { permisos para R_CLIENTE }
 
 GRANT SELECT
     ON autoracle.cliente
     TO r_cliente;
-
 
 GRANT SELECT
     ON autoracle.cita
@@ -86,6 +168,66 @@ GRANT SELECT
 GRANT SELECT
     ON autoracle.vehículo
     TO r_mecanico, r_cliente;
+
+-- agregamos una politica VPD (ver practica 3 / tema 2) para limitar el acceso a los datos de cada cliente
+-- agregamos restricciones a las tablas "CLIENTE", "CITA", "FACTURA" y "VEHICULO".
+SELECT * FROM ALL_CONSTRAINTS WHERE CONSTRAINT_NAME LIKE '%CLIENTE%';
+-- permite precisar qué tablas dependen de EMPLEADO_ID
+
+-- devuelve un filtro para la clausula WHERE
+-- https://www.techonthenet.com/oracle/functions/sys_context.php
+CREATE OR REPLACE FUNCTION AUTORACLE.SOLO_CLIENTE_ACTUAL_CLIENTE (p_esquema IN VARCHAR2, p_objeto IN VARCHAR2)
+    RETURN VARCHAR2 AS
+BEGIN
+    IF (SYS_CONTEXT('userenv', 'isdba') = 'TRUE') THEN
+        RETURN '';
+    ELSE
+        RETURN 'IDCLIENTE = ''' || SYS_CONTEXT('userenv', 'session_userid') | '''';
+    END IF;
+END;
+
+CREATE OR REPLACE FUNCTION AUTORACLE.SOLO_CLIENTE_ACTUAL_CITA_VEHIC_FAC (p_esquema IN VARCHAR2, p_objeto IN VARCHAR2)
+    RETURN VARCHAR2 AS
+BEGIN
+    IF (SYS_CONTEXT('userenv', 'isdba') = 'TRUE') THEN
+        RETURN '';
+    ELSE
+        RETURN 'CLIENTE_IDCLIENTE = ''' || SYS_CONTEXT('userenv', 'session_userid') || '''';
+    END IF;
+END;
+
+BEGIN
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'CLIENTE', policy_name => 'POL_CLIENTE_CLIENTE',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_CLIENTE_ACTUAL_CLIENTE',
+        statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'CITA', policy_name => 'POL_CLIENTE_CITA',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_CLIENTE_ACTUAL_CITA_VEHIC_FAC',
+        statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'VEHICULO', policy_name => 'POL_CLIENTE_VEHICULO',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_CLIENTE_ACTUAL_CITA_VEHIC_FAC',
+        statement_types => 'SELECT'
+    );
+    DBMS_RLS.ADD_POLICY (
+        object_schema => 'AUTORACLE', object_name => 'FACTURA', policy_name => 'POL_CLIENTE_FACTURA',
+        function_schema => 'AUTORACLE', policy_function => 'SOLO_CLIENTE_ACTUAL_CITA_VEHIC_FAC',
+        statement_types => 'SELECT'
+    );
+END;
+/
+
+-- en el caso de que te equivoques:
+BEGIN
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'CLIENTE', 'POL_CLIENTE_CLIENTE');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'CITA', 'POL_CLIENTE_CITA');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'VEHICULO', 'POL_CLIENTE_VEHICULO');
+    DBMS_RLS.DROP_POLICY('AUTORACLE', 'FACTURA', 'POL_CLIENTE_FACTURA');
+END;
+/
 
 
 -- [2]
