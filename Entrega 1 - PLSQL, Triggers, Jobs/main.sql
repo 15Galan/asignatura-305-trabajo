46db298b8bd2fdefb9cabefcbe85e1cf1d3a3e8a
unknown <quino.terrasa+dev@gmail.com>
2020-04-30 19:30:20 +0200

    He añadido la estructura del ejercicio 4. Tengo muchas dudas, y el enunciado esta mal explicado, asi que queda preguntarlo por el foro

--- Entrega 1 - PLSQL, Triggers, Jobs/main.sql
+++ Entrega 1 - PLSQL, Triggers, Jobs/main.sql
@@ -1,4 +1,4 @@
-P_-- Trabajo en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
+-- Trabajo en Grupo (PL/SQL, Triggers, Jobs), a 29 Abril 2020.
 -- Antonio J. Galán, Manuel González, Pablo Rodríguez, Joaquin Terrasa
 
 -- { Por defecto, usamos el usuario "AUTORACLE" creado previamente en la BD }
@@ -276,11 +276,68 @@ CREATE OR REPLACE VIEW AUTORACLE.V_IVA_TRIMESTRE AS
 SELECT * FROM AUTORACLE.V_IVA_TRIMESTRE;
 SELECT * FROM AUTORACLE.FACTURA;
 
--- [4]
-	
-	CREATE OR REPLACE
+-- [4] Crear un paquete en PL/SQL de análisis de datos.
+--       1. La función F_Calcular_Piezas devolverá la media, mínimo y máximo número de unidades compradas (en cada lote)
+--          de una determinada pieza en un año concreto.
+--       2. La función F_Calcular_Tiempos devolverá la media de días en las que se termina un servicio
+--          (Fecha de realización – Fecha de entrada en taller) así como la media de las horas de mano de obra de los
+--          servicios de Reparación.
+--       3. El procedimiento P_Recompensa encuentra el servicio proporcionado más rápido y más lento (en días) y a los
+--          empleados involucrados los recompensa con un +/- 5% en su sueldo base respectivamente.
 
--- [5]
+-- Primero, agrupamos las funciones y procedimientos en un paquete
+-- http://www.rebellionrider.com/how-to-create-pl-sql-packages-in-oracle-database/
+CREATE OR REPLACE PACKAGE PKG_AUTORACLE_ANALISIS AS
+    FUNCTION F_CALCULAR_PIEZAS(codref IN VARCHAR2, year in VARCHAR2) RETURN MEDIA_MIN_MAX_UNITS;
+    FUNCTION F_CALCULAR_TIEMPOS RETURN QUE_WEA_SE_DEVUELVE_PENDEJO;
+    PROCEDURE P_RECOMPENSA;
+END;
+
+-- Creamos todos los tipos propios que nos hagan falta
+CREATE OR REPLACE TYPE MEDIA_MIN_MAX_UNITS AS OBJECT (
+    media NUMBER,
+    minimo NUMBER,
+    maximo NUMBER);
+
+-- Creamos las funciones y procedimientos del paquete (RECOMIENDO probar a definirlas fuera del paquete para ver si 
+-- compilan, y despues borrarlas)
+CREATE OR REPLACE PACKAGE BODY PKG_AUTORACLE_ANALISIS IS
+    -- 1.
+    CREATE OR REPLACE FUNCTION F_CALCULAR_PIEZAS(codref IN VARCHAR2, year in VARCHAR2) RETURN MEDIA_MIN_MAX_UNITS AS
+        resultado MEDIA_MIN_MAX_UNITS;
+    BEGIN
+        -- Falta por hacer
+    END;
+
+    -- 2. Es una funcion, pero entiendo que si se usa para TODOS los servicios, entonces es mejor un Procedimiento.
+    -- Lo aclararia en el foro.
+    CREATE OR REPLACE FUNCTION F_CALCULAR_TIEMPOS RETURN QUE_WEA_SE_DEVUELVE_PENDEJO AS
+        dummy_var NUMBER;
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Do nothing');
+    END;
+
+    -- 3. VALE! Creo que este procedimiento usa las dos funciones de arriba para cada servicio. Entonces las funciones
+    -- van para cada servicio (No se explica bien en el enunciado).
+    CREATE OR REPLACE PROCEDURE P_RECOMPENSA AS
+        dummy_var NUMBER;
+    BEGIN
+        FOR numero IN (SELECT 1 FROM DUAL) LOOP
+            DBMS_OUTPUT.PUT_LINE('Do nothing');
+        END LOOP;
+    END;
+END;
+
+
+-- [5] Añadir al modelo una tabla FIDELIZACIÓN que permite almacenar un descuento por cliente y año. Crear un paquete en
+-- PL/SQL de gestión de descuentos.
+--       1. El procedimiento P_Calcular_Descuento, tomará un cliente y un año y calculará el descuento del que podrá
+--          disfrutar el año siguiente. Para ello, hasta un máximo del 10%, irá incrementando el descuento en un 1%,
+--          por cada una de las siguientes acciones:
+--              1. Por cada servicio pagado por el cliente.
+--              2. Por cada ocasión en la que el cliente tuvo que esperar más de 5 días desde que solicitó la cita hasta
+--                 que se concertó.
+--              3. Por cada servicio proporcionado en el que tuvo que esperar más de la media de todos los servicios.
 
 CREATE TABLE autoracle.fidelizacion(
     "CLIENTE" VARCHAR2(16),
@@ -339,11 +396,17 @@ END pck_gestion_descuentos;
 /
 			
 			
--- [6]
+-- [6] Crear un paquete en PL/SQL de gestión de empleados que incluya las operaciones para crear, borrar y modificar los
+-- datos de un empleado. Hay que tener en cuenta que algunos empleados tienen un usuario y, por tanto, al insertar o 
+-- modificar un empleado, si su usuario no es nulo, habrá que crear su usuario. Además, el paquete ofrecerá
+-- procedimientos para bloquear/desbloquear cuentas de usuarios de modo individual.
+-- También se debe disponer de una opción para bloquear y desbloquear todas las cuentas de los empleados.
 
--- [7]
+-- [7] Escribir un trigger que cuando se eliminen los datos de un cliente fidelizado se eliminen a su vez toda su
+-- información de fidelización y los datos de su vehículo.
 
--- [8]
+-- [8] Crear un JOB que ejecute el procedimiento P_REVISA todos los días a las 21:00. Crear otro JOB que, anualmente 
+-- (el 31 de diciembre a las 23.55), llame a P_Recompensa.
 
 	BEGIN
 		DBMS_SCHEDULER.CREATE_JOB
