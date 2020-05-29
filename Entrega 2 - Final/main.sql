23aa6e8fd8aac30e4fde59183f3289f43a65b09d
Galán <antoniogalanG15@gmail.com>
2020-05-29 03:06:29 +0200

    Se creó una rama para la Entrega 2
    Se añadió un script para trabajar en esta entrega.

---
+++ Entrega 2 - Final/main.sql
@@ -0,0 +1,7 @@
+-- Trabajo en Grupo (entrega final), a 29 Mayo 2020.
+-- Antonio J. Galan, Manuel Gonzalez, Pablo Rodriguez, Joaquin Terrasa
+
+-- Ver estado (ON / OFF) del paquete DBMS_OUTPUT
+-- SHOW SERVEROUTPUT;
+-- Activar la opcion de mostrar mensajes por pantalla (1 vez / sesion)
+SET SERVEROUTPUT ON;
